7f615f35901b900660c86ad64eb83ca1bf0719c9
Santiago <santiagobernal93@gmail.com>
2015-07-31 18:51:10 -0430

    Added user profile view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,11 @@ module UsersHelper
       base_title + " " + page_title
     end
   end
+
+ 	# Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
